8a467b3c1c95e768a628113f26de8811d654a736
Basudeb2005 <e1303027@u.nus.edu>
2025-01-26 21:50:08 +0800

    Changed the name of bot to VetBuddy

---
+++ src/main/java/VetBuddy.java
@@ -0,0 +1,16 @@
+public class VetBuddy {
+    public static void main(String[] args) {
+        String lineSeparator = "____________________________________________________________\n";
+        String greetingMessage = " Hello! I'm VetBuddy\n";
+        String actionPrompt = " What can I do for you?\n";
+        String farewellMessage = " Bye. Hope to see you again soon!\n";
+
+        System.out.print(lineSeparator);
+        System.out.print(greetingMessage);
+        System.out.print(actionPrompt);
+        System.out.print(lineSeparator);
+        System.out.print(farewellMessage);
+        System.out.print(lineSeparator);
+    }
+
+}
