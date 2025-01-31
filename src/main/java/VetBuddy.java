103ff9e56f7111cb3ad2cf31e8b5f2c16988799a
Basudeb2005 <e1303027@u.nus.edu>
2025-01-31 15:05:30 +0800

    Implement Level-2: Add, List functionality for VetBuddy chatbot

--- src/main/java/VetBuddy.java
+++ src/main/java/VetBuddy.java
@@ -7,6 +7,8 @@ public class VetBuddy {
         String greetingMessage = " Hello! I'm VetBuddy";
         String actionPrompt = " What can I do for you?";
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         System.out.println(lineSeparator);
         System.out.println(greetingMessage);
@@ -14,13 +16,25 @@ public class VetBuddy {
         System.out.println(lineSeparator);
 
         while (true) {
-            String input = scanner.nextLine();
+            String input = scanner.nextLine().trim();
 
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println(lineSeparator);
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println(lineSeparator);
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                System.out.println(lineSeparator);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(lineSeparator);
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println(lineSeparator);
+                System.out.println(" added: " + input);
+                System.out.println(lineSeparator);
             }
 
 
